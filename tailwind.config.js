fe71f98d2c9304860024c3fabd6d2e25dc21ed62
khadija zahoor <khadijazahoor596@gmail.com>
2022-08-29 15:51:28 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'Raleway': ['Raleway', 'sans-serif'] ,
+          'Montserrat': ['Montserrat', 'sans-serif'] 
+
+        },
+      },
+    },
+    
+    variants: {
+    },
+    plugins: [
+    ]
+  }
